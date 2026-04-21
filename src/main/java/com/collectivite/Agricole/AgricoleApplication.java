fa92ddf43b73f8a8aaf23882d5ed706ba6bdfe81
Heriniaina <ramaheritoheriniaina@.com>
2026-04-21 14:44:14 +0300

    ..

---
+++ src/main/java/com/collectivite/Agricole/AgricoleApplication.java
@@ -0,0 +1,13 @@
+package com.collectivite.Agricole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectivite/Agricole/AgricoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectivite.Agricole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
